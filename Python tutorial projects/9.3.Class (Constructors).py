51037b4c834da54746ee314ea783e701114dcd1f
PedroCorreia07 <pedro.drsc.07@gmail.com>
2023-03-16 02:41:55 +0000

    move 9.3

--- Python tutorial projects/9.3.Class (Constructors).py
+++ Python tutorial projects/9.3.Class (Constructors).py
@@ -1,14 +1,14 @@
 #The constructor "__init__()" alows to set unique values for class variables when we create an instance, 
 #instead of setting it in the class definition
 class Pokemon:
-    def __init__(self,color,name,sound): #add two parameters to the constructor
-        self.color=color
+    def __init__(self,color,name,sound): #add three parameters to the constructor| Just like with regular class methods we need to add self to the first parameter
+        self.color=color #By using the self keyword to refer to these variables, we are telling Python to assign the values of color, name, and sound to these instance variables of the current object.
         self.name=name
         self.sound=sound
-    def pokemon_sound(self):
+    def pokemon_sound(self): #create method for the pokemon sound
         print(self.sound)
-charizard=Pokemon("orange","charizard","roarrrr")
-print(charizard.color)
+charizard=Pokemon("orange","charizard","roarrrr") #create instance (3 parameters like above)
+print(charizard.color) #show parameters
 print(charizard.name)
-charizard.pokemon_sound()
+charizard.pokemon_sound() #show method
 
